9ad627d25459ab71c9a7c1c6c8fe3c6783a63c4c
Jean Pier Carrasco Tamariz <jeanpi.jpct@gmail.com>
2023-10-06 17:09:47 -0500

    server

--- app/Models/OrdenServicio.php
+++ app/Models/OrdenServicio.php
@@ -28,18 +28,6 @@ class OrdenServicio extends Model
         ->join("clientes","orden_servicio.id_cliente","=","clientes.id")
         ->get();
     }
-    public function scopeDatosCotizaciones($query,$idOrdenServicio) {
-        return $query->select("orden_servicio_cotizacion.id AS idOsCotizacion","servicios.servicio","cotizacion_servicio.costo","cotizacion_servicio.cantidad","cotizacion_servicio.id AS idCotizacionServicio","cotizacion_servicio.importe","cotizacion_servicio.igv","cotizacion_servicio.descuento","cotizacion_servicio.total")
-        ->selectRaw("DATE_FORMAT(orden_servicio.fecha,'%d/%m/%Y') AS fechaOs,LPAD(cotizacion_servicio.id_cotizacion,5,'0') AS nroCotizacion")
-        ->join("orden_servicio_cotizacion","orden_servicio_cotizacion.id_orden_servicio","=","orden_servicio.id")
-        ->join("cotizacion_servicio","cotizacion_servicio.id","=","orden_servicio_cotizacion.id_cotizacion_servicio")
-        ->join("servicios","servicios.id","=","cotizacion_servicio.id_servicio")
-        ->where('id_orden_servicio',$idOrdenServicio)
-        ->groupBy("cotizacion_servicio.id_cotizacion")
-        ->groupBy("cotizacion_servicio.id_servicio")
-        ->orderBy('cotizacion_servicio.id')
-        ->get();
-    }
     public function servicios() {
         return $this->hasMany(OrdenServicioCotizacionServicio::class,'id_orden_servicio');
     }
